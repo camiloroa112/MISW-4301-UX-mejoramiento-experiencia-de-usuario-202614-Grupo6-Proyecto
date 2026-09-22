4d997b658a41cb98d5407aeb5533b5881596783f
camiloroa112 <camiloroa112@gmail.com>
2026-09-22 01:03:56 -0500

    feature (m03-reto-faltan): Creación de RF junto con refactoring.

--- mobile/src/app/reto-faltan/reto-faltan.page.spec.ts
+++ mobile/src/app/reto-faltan/reto-faltan.page.spec.ts
@@ -1,16 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// Librerías
+import { provideRouter } from '@angular/router';
 import { RetoFaltanPage } from './reto-faltan.page';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// Configuración de los tests
 describe('RetoFaltanPage', () => {
+
   let component: RetoFaltanPage;
   let fixture: ComponentFixture<RetoFaltanPage>;
 
-  beforeEach(() => {
+  // Inicialización de los tests
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RetoFaltanPage],
+      providers: [
+        provideRouter([]),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RetoFaltanPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  // Validación del componente
   it('should create', () => {
     expect(component).toBeTruthy();
   });
